0d7de1ab178c99fa84e96a7398e6a9c3b195d4c7
sherrylxf <1298512407@qq.com>
2025-08-06 12:50:51 +0800

    更新所有实验的code

---
+++ code/lock/kernel/kalloc.c
@@ -0,0 +1,111 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+#define NCPU 8  // 假设最多 8 核心
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem[NCPU];
+
+void
+kinit()
+{
+  for (int i = 0; i < NCPU; i++) {
+    char name[16];
+    snprintf(name, sizeof(name), "kmem%d", i);
+    initlock(&kmem[i].lock, name);
+  }
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p = (char*)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    kfree(p);
+}
+
+
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  memset(pa, 1, PGSIZE);
+  struct run *r = (struct run*)pa;
+
+  push_off(); // 禁中断，获取 cpuid 安全
+  int id = cpuid();
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+  pop_off();
+}
+
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r = 0;
+
+  push_off();
+  int id = cpuid();
+
+  // Step 1: 尝试从本 CPU 分配
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
+  if (r)
+    kmem[id].freelist = r->next;
+  release(&kmem[id].lock);
+
+  // Step 2: 偷窃其他 CPU freelist
+  if (r == 0) {
+    for (int i = 0; i < NCPU; i++) {
+      if (i == id)
+        continue;
+
+      acquire(&kmem[i].lock);
+      if (kmem[i].freelist) {
+        r = kmem[i].freelist;
+        kmem[i].freelist = r->next;
+        release(&kmem[i].lock);
+        break;
+      }
+      release(&kmem[i].lock);
+    }
+  }
+
+  pop_off();
+
+  if (r)
+    memset((char*)r, 5, PGSIZE); // 填充 junk
+  return (void*)r;
+}
